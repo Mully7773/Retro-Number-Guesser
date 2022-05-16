5816760f3087f6e9bb963faff2ed344775e722bc
Nick M <mully7773@gmail.com>
2022-05-16 13:31:42 -0500

    Added secret number generator and simple click events

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!😁';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+//the function below is an event handler called when the event happens
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess); //returns 'string' if Number function is not used
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Invalid number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'You got it!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
+  }
+});
